80e62f27e2ff40c71bc7095a305ea218f40a403d
Graham White <grazmo@gmail.com>
2018-01-14 15:35:24 +1100

    Added dissimilarity unit tests

---
+++ tests/testthat/test-dissimilarity.R
@@ -0,0 +1,54 @@
+context("dissimilarity measures")
+
+load(system.file("testdata","testData_dissimilarity.RData",package = "IndexNumR"))
+
+test_that("Dissimilarity measures return the correct values",{
+  expect_equal(relativeDissimilarity(CES_sigma_2, pvar="prices",qvar="quantities",
+                                     pervar="time",prodID = "prodID",indexMethod = "fisher",
+                                     similarityMethod = "logquadratic"), testData$lq)
+  expect_equal(relativeDissimilarity(CES_sigma_2, pvar="prices",qvar="quantities",
+                                     pervar="time",prodID = "prodID",indexMethod = "fisher",
+                                     similarityMethod = "asymplinear"), testData$ab)
+  expect_equal(relativeDissimilarity(CES_sigma_2, pvar="prices",qvar="quantities",
+                                     pervar="time",prodID = "prodID",indexMethod = "fisher",
+                                     similarityMethod = "plspread"), testData$pl)
+  expect_equal(mixScaleDissimilarity(CES_sigma_2, pvar="prices",qvar="quantities",pervar="time",
+                                     prodID = "prodID",measure = "absolute", combine = "geomean"),
+               testData$abs)
+  expect_equal(mixScaleDissimilarity(CES_sigma_2, pvar="prices",qvar="quantities",pervar="time",
+                                     prodID = "prodID",measure = "mix", combine = "geomean"),
+               testData$mix)
+  expect_equal(mixScaleDissimilarity(CES_sigma_2, pvar="prices",qvar="quantities",pervar="time",
+                                     prodID = "prodID",measure = "scale", combine = "geomean"),
+               testData$scale)
+})
+
+test_that("maximum similarity links are estimated correctly",{
+  expect_equal(maximiumSimilarityLinks(testData$lq), testData$maxlinks)
+})
+
+test_that("similarity linked bilateral price index functions return the correct values",{
+  expect_equal(priceIndex(CES_sigma_2,pvar = "prices",qvar = "quantities",pervar = "time",
+                          prodID = "prodID",indexMethod = "laspeyres", sample="matched",
+                          output = "chained", chainMethod = "logquadratic"), testData$p_lq)
+  expect_equal(priceIndex(CES_sigma_2,pvar = "prices",qvar = "quantities",pervar = "time",
+                          prodID = "prodID",indexMethod = "laspeyres", sample="matched",
+                          output = "chained", chainMethod = "asymplinear"), testData$p_ab)
+  expect_equal(priceIndex(CES_sigma_2,pvar = "prices",qvar = "quantities",pervar = "time",
+                          prodID = "prodID",indexMethod = "laspeyres", sample="matched",
+                          output = "chained", chainMethod = "plspread"), testData$p_pl)
+  expect_equal(priceIndex(CES_sigma_2,pvar = "prices",qvar = "quantities",pervar = "time",
+                          prodID = "prodID",indexMethod = "laspeyres", sample="matched",
+                          output = "chained", chainMethod = "mixscale", measure="absolute")
+               , testData$p_abs)
+  expect_equal(priceIndex(CES_sigma_2,pvar = "prices",qvar = "quantities",pervar = "time",
+                          prodID = "prodID",indexMethod = "laspeyres", sample="matched",
+                          output = "chained", chainMethod = "mixscale", measure="mix")
+               , testData$p_mix)
+  expect_equal(priceIndex(CES_sigma_2,pvar = "prices",qvar = "quantities",pervar = "time",
+                          prodID = "prodID",indexMethod = "laspeyres", sample="matched",
+                          output = "chained", chainMethod = "mixscale", measure="scale")
+               , testData$p_scale)
+})
+
+rm(testData)
